3ca74bb760272d435f4f2fc4131ef3a60a241e27
ajayreddykommera <ajayreddykommera@gmail.com>
2024-07-14 20:50:13 -0500

    fix: added claims to jwt token

--- src/main/java/com/ajay/blog_app/security/jwt/JwtUtils.java
+++ src/main/java/com/ajay/blog_app/security/jwt/JwtUtils.java
@@ -10,10 +10,12 @@ import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -28,9 +30,13 @@ public class JwtUtils {
     public String generateJwtToken(Authentication authentication) {
 
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
         long expirationTime = System.currentTimeMillis() + jwtExpiration;
         return Jwts.builder()
                 .subject(userPrincipal.getUsername())
+                .claim("roles", roles)
                 .issuedAt(new Date())
                 .expiration(new Date(expirationTime))
                 .signWith(key())
